42b066ad5967bb6c50e48277c7ed5f1523e86434
bri111 <brianall@hawaii.edu>
2025-09-29 18:04:18 -1000

    indentions

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -9,19 +9,19 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(-1, -1), -2)
 
 
-def test_subtract(self):
-    self.assertEqual(subtract(5, 3), 2)
-    self.assertEqual(subtract(0, 0), 0)
-    self.assertEqual(subtract(-1, -1), 0)
-    self.assertEqual(subtract(-1, 1), -2)
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(0, 0), 0)
+        self.assertEqual(subtract(-1, -1), 0)
+        self.assertEqual(subtract(-1, 1), -2)
 
 
-# COMPLETE HERE
-def test_divide(self):
-    self.assertEqual(divide(6, 3), 2)
-    self.assertEqual(divide(5, 2), 2.5)
-    self.assertEqual(divide(-6, -3), 2)
-    self.assertEqual(divide(-6, 3), -2)
+    # COMPLETE HERE
+    def test_divide(self):
+        self.assertEqual(divide(6, 3), 2)
+        self.assertEqual(divide(5, 2), 2.5)
+        self.assertEqual(divide(-6, -3), 2)
+        self.assertEqual(divide(-6, 3), -2)
 
 
 # COMPLETE HERE
